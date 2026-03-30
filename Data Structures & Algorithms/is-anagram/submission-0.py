1738bbc1103684e401d30fe3c1025326def02428
Joseph Mpo Yeti <55380155+joseph-mpo-yeti@users.noreply.github.com>
2026-03-30 20:21:47 +0100

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        chars = { l:0 for l in s }
+
+        for l in s:
+            chars[l] += 1
+
+        for l in t:
+            if not l in chars:
+                return False
+            chars[l] -= 1
+
+        for k in chars:
+            if chars[k] > 0:
+                return False
+        
+        return True
